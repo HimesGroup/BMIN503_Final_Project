f541ec837d4bf84ed41fd61cdbd0522199600a43
STEPHANIE TEEPLE <steeple@pmacs.upenn.edu>
2018-12-07 17:19:57 -0500

    Formatted date variables, for the time being. Started constructing new pub_count and impact_factor variables, but am temporarily stumped.

--- 01_data_clean.R
+++ 01_data_clean.R
@@ -40,7 +40,37 @@ for (i in numeric_vars){
 }
 sapply(data, function(x) sum(is.na(x)))
 
-# 4. Check distributions 
-summary(data)
+# 4. Date variables processing  
 
+  # PUB_DATE
+  # Some observations only have month + year, and some only year. 
+  # Fill in with the 15th day of each and June 15th. 
+  # TODO: imputation for date missingness???
+  data$PUB_DATE_addn <- ""
+  data$PUB_DATE_addn[nchar(data$PUB_DATE) < 9 & nchar(data$PUB_DATE) > 4] <- " 15"
+  data$PUB_DATE_addn[nchar(data$PUB_DATE) == 4] <- " Jun 15"
+  data <- mutate(data, PUB_DATE = paste0(PUB_DATE, PUB_DATE_addn))
+  data$PUB_DATE_addn <- NULL
+  
+    # TODO: There are many non-standard date formats in this variable (~40,000 rows)
+    # Example: yyyy mmm-mmm (2001 Apr-May) and yyyy season-season (2002 Fall-Wi)
+    # For expediency's sake, leave as missing for time being 
+    data$PUB_DATE <- ymd(data$PUB_DATE)
+    
+  # BUDGET_START and BUDGET_END
+  data$BUDGET_START <- mdy(data$BUDGET_START)
+  data$BUDGET_END <- mdy(data$BUDGET_END)
 
+# 5. Constructing new variables 
+  
+  # Publication counts 
+  # NOTE: each APPLICATION_ID corresponds to a unique FULL_PROJECT_NUM.
+  # We want the number of publications for each PI before a particular FULL_PROJECT_NUM
+  # was funded. 
+  pub_counts <- test %>%
+    group_by(PI_IDS) %>%
+    summarize(pub_counts = n())
+  test <- full_join(test, pub_counts, by ="PI_IDS")  # Counts of publications per PI/Project combo
+  
+  # No... I think we want the count of ALL publications for a PI for a period defined by projects. 
+  groups <- group_by(data, )
