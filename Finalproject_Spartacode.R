922b2bf295638d3407f6ba26b000889a70597246
Bernadette D'Alonzo <dalonzob@pennmedicine.upenn.edu>
2020-11-17 17:43:04 -0500

    Updated final project Rmd w/ code from R script and removed duplicated README file

--- Finalproject_Spartacode.R
+++ Finalproject_Spartacode.R
@@ -53,46 +53,71 @@ print(scans.injury.riskscore)
 
 #This shows most common injury risk score is 1 (then 3, 0, 5, 2)
 ggplot(data = sparta.scans.clean, aes(x = injury.riskscore)) +
-  geom_bar()
+  geom_bar() +
+  labs(y = "Scans (count)",
+     x = "Injury risk score (0-5)") +
+  ggtitle("# Scans by Injury Risk Score")
 
 #This shows variability in scans by injury risk score both within the same athlete and across the 35 M lacrosse athletes. 
 ggplot(data = sparta.scans.clean, aes(y = id.new, fill = injury.riskscore)) +
-  geom_bar()
+  geom_bar() +
+  labs(y = "Athlete ID",
+     x = "Scans (counts)",
+     fill = "Injury risk score(0-5)") +
+  ggtitle("Variability in Injury Risk Score within and across athletes")
 
 #Jump height and injury risk score
+#ggplot(data = sparta.scans.clean, aes(x = jump.ht.in, y = injury.riskscore)) +
+  #geom_point()
 ggplot(data = sparta.scans.clean, aes(x = jump.ht.in, y = injury.riskscore)) +
-  geom_point()
-ggplot(data = sparta.scans.clean, aes(x = jump.ht.in, y = injury.riskscore)) +
-  geom_boxplot()
-
+  geom_boxplot() +
+  labs(y = "Injury risk score",
+        x = "Jump height (in)") +
+  ggtitle("Jump height (in) and Injury risk score")
 
 #This shows median, IQR of LOAD, EXPLODE, DRIVE tscores within and across all M lacrosse athletes
 #LOAD
 #ggplot(data = sparta.scans.clean, aes(x = load.tscore, y = id.new)) +
   #geom_point()
 ggplot(data = sparta.scans.clean, aes(x = load.tscore, y = id.new)) +
-  geom_boxplot()
+  geom_boxplot() +
+  labs(y = "Athlete ID",
+         x = "Load t-score") +
+  ggtitle("Variability in Load within and across athletes")
 
 #EXPLODE
 #ggplot(data = sparta.scans.clean, aes(x = explode.tscore, y = id.new)) +
   #geom_point()
 ggplot(data = sparta.scans.clean, aes(x = explode.tscore, y = id.new)) +
-  geom_boxplot()
+  geom_boxplot() +
+  labs(y = "Athlete ID",
+         x = "Explode t-score") +
+  ggtitle("Variability in Explode within and across athletes")
 
 #DRIVE
 #ggplot(data = sparta.scans.clean, aes(x = drive.tscore, y = id.new)) +
   #geom_point()
 ggplot(data = sparta.scans.clean, aes(x = drive.tscore, y = id.new)) +
-  geom_boxplot()
-
+  geom_boxplot() +
+  labs(y = "Athlete ID",
+        x = "Drive t-score") +
+  ggtitle("Variability in Drive within and across athletes")
 
 #Scatterplots - LOAD, EXPLODE, DRIVE and injury.riskscore
 ggplot(sparta.scans.clean, aes(x = load.tscore, y = explode.tscore,)) +
-  geom_point(aes(color=factor(injury.riskscore)))
+  geom_point(aes(color=factor(injury.riskscore))) +
+  labs(y = "Explode t-score",
+        x = "Load t-score")
 ggplot(sparta.scans.clean, aes(x = explode.tscore, y = drive.tscore,)) +
-  geom_point(aes(color=factor(injury.riskscore)))
+  geom_point(aes(color=factor(injury.riskscore))) +
+  labs(y = "Drive t-score",
+         x = "Explode t-score")
 ggplot(sparta.scans.clean, aes(x = load.tscore, y = drive.tscore,)) +
-  geom_point(aes(color=factor(injury.riskscore)))
+  geom_point(aes(color=factor(injury.riskscore))) +
+  labs(y = "Drive t-score",
+        x = "Load t-score")
+  
+
 
 
 #Splitting LOAD, EXPLODE, DRIVE tscores into tertiles - low, med, high and added these columns to df
@@ -107,19 +132,37 @@ sparta.scans.clean <- sparta.scans.clean %>%
 #concatenate their "movement signature" into a "jump profile"
 sparta.scans.clean$jump.profile <- paste(sparta.scans.clean$load.tert,sparta.scans.clean$explode.tert,sparta.scans.clean$drive.tert)
 
-#This shows variability in LOAD, EXPLODE, DRIVE within and across 
+#This shows variability in LOAD, EXPLODE, DRIVE within and across athletes
 #Load
 ggplot(data = sparta.scans.clean, aes(y = id.new, fill = load.tert)) +
-  geom_bar()
+  geom_bar() +
+  labs(y = "Athlete ID",
+       x = "Scans (counts)",
+       fill = "Load tertiles") +
+  ggtitle("Variability in Load within and across athletes")
+
 #Explode
 ggplot(data = sparta.scans.clean, aes(y = id.new, fill = explode.tert)) +
-  geom_bar()
+  geom_bar() +
+  labs(y = "Athlete ID",
+       x = "Scans (counts)",
+       fill = "Explode tertiles") +
+  ggtitle("Variability in Explode within and across athletes") 
+
 #Drive
 ggplot(data = sparta.scans.clean, aes(y = id.new, fill = drive.tert)) +
-  geom_bar()
-
+  geom_bar() +
+  labs(y = "Athlete ID",
+       x = "Scans (counts)",
+       fill = "Drive tertiles") +
+  ggtitle("Variability in Drive within and across athletes")
 
 #This shows the variability in jump profile within and across athletes
 ggplot(data = sparta.scans.clean, aes(y = id.new, fill = jump.profile)) +
-  geom_bar()
+  geom_bar() +
+  labs(y = "Athlete ID",
+       x = "Scans (counts)",
+       fill = "Jump profile - LOAD, EXPLODE, DRIVE") +
+  ggtitle("Variability in jump profile within and across athletes")
 
+       
