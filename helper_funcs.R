b68d1f4f18cd8a92d060b2efb5f22926b5dc4d30
syhwng <77501546+syhwng@users.noreply.github.com>
2021-11-23 15:13:27 -0500

    Add helper funcs

---
+++ helper_funcs.R
@@ -0,0 +1,17 @@
+
+library(data.table)
+library(dplyr)
+library(sf)
+library(readr)
+
+
+sfc_as_cols <- function(x, names = c("lon","lat")) {
+  stopifnot(inherits(x,"sf") && inherits(sf::st_geometry(x),"sfc_POINT"))                                                                                                                                                                                                                                                     
+  ret <- sf::st_coordinates(x)
+  ret <- tibble::as_tibble(ret)
+  stopifnot(length(names) == ncol(ret))
+  x <- x[ , !names(x) %in% names]
+  ret <- setNames(ret,names)
+  ui <- dplyr::bind_cols(x,ret)
+  st_set_geometry(ui, NULL)
+}
