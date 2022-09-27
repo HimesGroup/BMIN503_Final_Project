a4dcfe8e92a7690033049c4ec4eea53e9710a91a
David Roberts <10406639+dr00b@users.noreply.github.com>
2022-09-27 17:21:27 -0400

    Implemented zip extract for nhc_provider_info

--- transform/nhc_provider_info.py
+++ transform/nhc_provider_info.py
@@ -21,12 +21,19 @@ FILE_OUT = os.path.join("data", "interim", "provider_info_tmp.csv")
 COL_MAPPING = json.load(open(os.path.join(CWD, "transform", "mappings", "nhc_provider_info.json")))
 COL_MAPPING = {k.lower().replace(" ", "_"): val.lower().replace(" ", "_") for k, val in COL_MAPPING.items()}
 ALL_COLS = set(COL_MAPPING.keys()).union(set(COL_MAPPING.values()))
-COL_COUNT = 34
+COL_COUNT = 35
+FINAL_COL_LST = list(COL_MAPPING.values())[0:COL_COUNT]
+COL_MAPPING.update({v: v for v in COL_MAPPING.values()})
+ALLOWED_OMMISSIONS = {
+    # not present in 2022-07 data, replaced by turnover?
+    'rn_staffing_rating_footnote',
+    'rn_staffing_rating'
+}
 
-def main():
-    with open(FILE_OUT, "w") as f:
-        writer = csv.writer(f)
-        writer.writerow(list(COL_MAPPING.keys()) + ["source_file"])
+def extract_zips():
+    with open(FILE_OUT, "w", newline='') as f:
+        writer = csv.writer(f, delimiter='|')
+        writer.writerow(FINAL_COL_LST + ["source_file"])
     for archive in RAW_DATA_LST:
         with ZipFile(archive, 'r') as zip1:
             file_lst = zip1.namelist()
@@ -38,24 +45,43 @@ def main():
                     assert len(lst) == 1
                     file_name = lst[0]
                     zip2.extract(file_name, "_tmp")
-                with open(os.path.join("_tmp", file_name), "r") as fi:
+                with open(os.path.join("_tmp", file_name), "r", encoding='latin-1') as fi:
+                    missing_cols = {}
                     reader = csv.reader(fi)
                     header = [h.lower().replace(" ", "_") for h in next(reader)]
-                    indexes = [i for i, col in enumerate(header) if col in ALL_COLS]
-                    if len(indexes) != COL_COUNT:
+                    mapped_cols = {}
+                    for i, col in enumerate(header):
+                        mapped_col = COL_MAPPING.get(col, None)                         
+                        if mapped_col:
+                            mapped_cols[mapped_col] = i
+                    if len(mapped_cols) != COL_COUNT:
                         print([col for col in header if col not in ALL_COLS])
-                        print(len(indexes))
-                        raise(Exception)
-                    with open(FILE_OUT, "a") as fo:
-                        writer = csv.writer(fo)
+                        print(len(mapped_cols))
+                        missing_cols = set(FINAL_COL_LST) - set(mapped_cols.keys())
+                        print(missing_cols)
+                        if len(missing_cols - ALLOWED_OMMISSIONS) > 0:
+                            raise(Exception)
+                        for col in missing_cols:
+                            mapped_cols[col] = None
+                    with open(FILE_OUT, "a", encoding='latin-1', newline='') as fo:
+                        writer = csv.writer(fo, delimiter='|')
                         count = 0
                         for row in reader:
                             if len(row) == 0:
                                 break
-                            writer.writerow([row[i] for i in indexes] + [f])
+                            row_to_write = []
+                            for col in FINAL_COL_LST:
+                                idx = mapped_cols.get(col, None)
+                                if idx is not None:
+                                    row_to_write.append(row[idx])
+                                else:
+                                    row_to_write.append("")
+                            row_to_write += [f]
+                            writer.writerow(row_to_write)
                             count+=1
                         print(f"{f}.{file_name}: {count} rows written")
                 shutil.rmtree("_tmp")
 
 if __name__ == '__main__':
-    main()
+    extract_zips()
+    
